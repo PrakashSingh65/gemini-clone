390d549402bd4db830e6519326a32260e7627a25
PrakashSingh65 <pratapsing5656@gmail.com>
2026-01-16 23:27:32 +0530

    Refactor Home component: update image import path and comment out unused CSS import

--- src/components/mai/Home.jsx
+++ src/components/mai/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import './main.css'
+//import './main.css'
+import userIcon from '../../assets/user_icon.png' // Adjust the path based on your folder structure
+
 export const Home = () => {
   return (
     <div className='main'>
       <div className='nav'>
         <p>Gemini</p>
-        <img src={"./assets/user_icon.png"} alt="user" />
+        <img src={userIcon} alt="user" />
       </div>
-        
-
     </div>
   )
 }
