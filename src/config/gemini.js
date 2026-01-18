0da5e1ea02a364d36944245e3ab76eac44a30a1c
PrakashSingh65 <pratapsing5656@gmail.com>
2026-01-18 22:14:53 +0530

    refactor: update AI configuration to use environment variable for API key and adjust model version

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -2,15 +2,14 @@
 
 
 import { GoogleGenAI } from "@google/genai";
+const APIKEY = import.meta.env.GOOGLE_API_KEY;
+const ai = new GoogleGenAI({ apiKey: APIKEY });
 
-const ai = new GoogleGenAI({ apiKey: "AIzaSyBLEOsLmagK9GIwhTSNt0K4ZpzeQhYKzXk" });
-
-async function main() {
+export async function GenerateAI() {
   const response = await ai.models.generateContent({
-    model: "gemini-3-flash-preview",
+    model: "gemini-2.5-flash",
     contents: "Explain how AI works in a few words",
   });
   console.log(response.text);
 }
 
-main();
