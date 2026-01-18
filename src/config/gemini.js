50e1728e4574cdf5c11e4ae793e2deaab84accc4
PrakashSingh65 <pratapsing5656@gmail.com>
2026-01-19 00:40:00 +0530

    feat: implement context provider and integrate chat functionality with safety settings

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,15 +1,48 @@
 
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+} from '@google/generative-ai';
 
+const MODEL_NAME = "gemini-1.0-pro";
+const API_KEY = "AIzaSyDUZOwpWKO2LSa3Zak7QOhhBXAssZ6bePw";
 
-import { GoogleGenAI } from "@google/genai";
-const APIKEY = import.meta.env.GOOGLE_API_KEY;
-const ai = new GoogleGenAI({ apiKey: APIKEY });
+async function runChat(prompt) {
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({ model: MODEL_NAME });
 
-export async function GenerateAI() {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: "Explain how AI works in a few words",
-  });
-  console.log(response.text);
+    const generationConfig = {
+        temperature: 0.9,
+        topK: 1,
+        topP: 1,
+        maxOutputTokens: 2048,
+    };
+
+    const safetySettings = [
+        {
+            category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        {
+            category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        {
+            category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+    ];
+
+    const chat = model.startChat({
+        generationConfig,
+        safetySettings,
+        history: [],
+    });
+
+    const result = await chat.sendMessage(prompt);
+    const response = result.response;
+    console.log(response.text());
 }
 
+export default runChat;
---
+++ src/context/context.jsx
@@ -0,0 +1,25 @@
+import { createContext } from "react";
+import runChat from "../config/gemini";
+
+export const context = createContext();
+
+const contextProvider = (props) =>{
+
+    const onSent = async (prompt) => {
+        runChat(prompt);
+    }
+
+    onSent("Hello, how are you?");
+
+
+    const contextValue ={
+
+    }
+    return (
+        context.Provider value={contextValue}>
+            {props.children}
+        </context.Provider>
+    )
+}
+
+export default contextProvider;
